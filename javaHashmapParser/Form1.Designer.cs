ed1455c9f6cca53be1155a9d97739e1b964b47c8
rudolph123 <rudolph123@interpark.com>
2021-03-19 17:28:20 +0900

    초기 커밋.

---
+++ javaHashmapParser/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace javaHashmapParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbDivide = new System.Windows.Forms.TextBox();
+            this.tbSplit = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 367);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(176, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(612, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Find Key";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(321, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(141, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Find Value";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbDivide
+            // 
+            this.tbDivide.Location = new System.Drawing.Point(132, 12);
+            this.tbDivide.Name = "tbDivide";
+            this.tbDivide.Size = new System.Drawing.Size(38, 21);
+            this.tbDivide.TabIndex = 7;
+            this.tbDivide.Text = "=";
+            this.tbDivide.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbSplit
+            // 
+            this.tbSplit.Location = new System.Drawing.Point(93, 12);
+            this.tbSplit.Name = "tbSplit";
+            this.tbSplit.Size = new System.Drawing.Size(33, 21);
+            this.tbSplit.TabIndex = 8;
+            this.tbSplit.Text = ",";
+            this.tbSplit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbSplit);
+            this.Controls.Add(this.tbDivide);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbDivide;
+        private System.Windows.Forms.TextBox tbSplit;
+    }
+}
+
---
+++ javaHashmapParser/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace javaHashmapParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String parseString = @"{a=0, b=1000, c=가나다라마바사}";
+            //DataTable ret = toDictionary(parseString);
+
+            dataGridView1.DefaultCellStyle.Font = new Font("Tahoma", 8);
+            dataGridView1.DefaultCellStyle.ForeColor = Color.Blue;
+            dataGridView1.DefaultCellStyle.BackColor = Color.Beige;
+            dataGridView1.DefaultCellStyle.SelectionForeColor = Color.Yellow;
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.Black;
+            textBox1.Text = parseString;
+        }
+
+        public DataTable toDictionary(String parseString)
+        {
+            DataTable dict = new DataTable();
+            dict.Columns.Add("KEY", typeof(String));
+            dict.Columns.Add("VALUE", typeof(String));
+
+            if (parseString == "") return null;
+
+            parseString = parseString.Trim();
+            //if (parseString.Substring(0, 1) != "{" &&
+            //        parseString.Substring(parseString.Length - 1, 1) != "}") {
+            //    return null;
+            //}
+            //parseString = parseString.Substring(1, parseString.Length - 2);
+            
+            String[] splitData = parseString.Split(new String[] { tbSplit.Text }, StringSplitOptions.RemoveEmptyEntries);
+            for(int i = 0; i < splitData.Length; i++)
+            {                
+                String[] splitItem = splitData[i].Split(new String[] { tbDivide.Text }, StringSplitOptions.None);
+                if(splitItem.Length == 2)
+                {
+                    DataRow dr = dict.NewRow();
+                    dr[0] = splitItem[0].Trim();
+                    dr[1] = splitItem[1].Trim();                    
+                    dict.Rows.Add(dr);
+                } else
+                {
+                    DataRow dr = dict.NewRow();
+                    dr[0] = splitItem[0].Trim();
+                    dr[1] = "";                    
+                    dict.Rows.Add(dr);
+                }
+            }
+
+            return dict;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+            DataTable dtRet = toDictionary(textBox1.Text);
+            dataGridView1.DataSource = dtRet;
+            
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.BorderStyle = BorderStyle.Fixed3D;
+            dataGridView1.EditMode = DataGridViewEditMode.EditOnEnter;
+
+
+            DataGridViewButtonColumn btnEncoding = new DataGridViewButtonColumn();
+            btnEncoding.HeaderText = "Encoding";
+            btnEncoding.Name = "ENC";
+            dataGridView1.Columns.Insert(1, btnEncoding);
+
+            DataGridViewButtonColumn btnDecoding = new DataGridViewButtonColumn();
+            btnDecoding.HeaderText = "Decoding";
+            btnDecoding.Name = "DES";
+            dataGridView1.Columns.Insert(2, btnDecoding);
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Cells[1].Value = "ENC";
+                row.Cells[2].Value = "DES";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable ret = (DataTable)dataGridView1.DataSource;
+
+            IEnumerable<DataRow> queryKey = from p in ret.AsEnumerable()
+                                            where p.Field<String>("KEY").StartsWith(textBox2.Text.Trim())
+                                            select p;
+            try
+            {
+                //dataGridView1.Columns.Clear();
+                dataGridView1.DataSource = queryKey.CopyToDataTable<DataRow>();
+
+                if (dataGridView1.Columns["ENC"] != null)
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        row.Cells["ENC"].Value = "ENC";
+                        row.Cells["DES"].Value = "DES";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataTable ret = (DataTable)dataGridView1.DataSource;
+
+            IEnumerable<DataRow> queryKey = from p in ret.AsEnumerable()
+                                            where p.Field<String>("VALUE").StartsWith(textBox3.Text.Trim())
+                                            select p;
+            try
+            {
+                //dataGridView1.Columns.Clear();
+                dataGridView1.DataSource = queryKey.CopyToDataTable<DataRow>();
+
+                if (dataGridView1.Columns["ENC"] != null)
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        row.Cells["ENC"].Value = "ENC";
+                        row.Cells["DES"].Value = "DES";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.ColumnIndex == -1 || e.RowIndex == -1) return;
+
+            if(dataGridView1.Columns[e.ColumnIndex].Name == "ENC")
+            {
+                String value = dataGridView1["VALUE", e.RowIndex].Value.ToString();
+                dataGridView1["VALUE", e.RowIndex].Value = System.Web.HttpUtility.UrlEncode(value);
+            }
+            else if(dataGridView1.Columns[e.ColumnIndex].Name == "DES")
+            {
+                String value = dataGridView1["VALUE", e.RowIndex].Value.ToString();
+                dataGridView1["VALUE", e.RowIndex].Value = System.Web.HttpUtility.UrlDecode(value);
+            }
+            dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Cells["ENC"].Value = "ENC";
+                row.Cells["DES"].Value = "DES";
+            }
+        }
+    }
+}
